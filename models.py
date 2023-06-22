1fec6903c1271dc342c6d9560a35508a0cc0a723
yihuitang <yihuitang@gmail.com>
2023-06-22 14:20:12 +0800

    update ProsodyPredictor
    concatenating the prosody embedding with the text embedding

--- models.py
+++ models.py
@@ -502,18 +502,21 @@ class LinearNorm(torch.nn.Module):
 
 class ProsodyPredictor(nn.Module):
 
-    def __init__(self, style_dim, d_hid, nlayers, dropout=0.1):
-        super().__init__() 
-        
-        self.text_encoder = DurationEncoder(sty_dim=style_dim, 
+    def __init__(self, n_prods, prod_embd, style_dim, d_hid, nlayers, dropout=0.1):
+        super().__init__()
+        self.embedding = nn.Embedding(n_prods, prod_embd * 2)
+        self.text_encoder = DurationEncoder(sty_dim=style_dim,
                                             d_model=d_hid,
-                                            nlayers=nlayers, 
+                                            nlayers=nlayers,
                                             dropout=dropout)
 
         self.lstm = nn.LSTM(d_hid + style_dim, d_hid // 2, 1, batch_first=True, bidirectional=True)
         self.duration_proj = LinearNorm(d_hid, 1)
-        
-        self.shared = nn.LSTM(d_hid + style_dim, d_hid // 2, 1, batch_first=True, bidirectional=True)
+
+        self.lstm = nn.LSTM(d_hid + prod_embd * 2 + style_dim, d_hid // 2, 1, batch_first=True, bidirectional=True)
+        self.duration_proj = LinearNorm(d_hid, 1)
+
+        self.shared = nn.LSTM(d_hid + prod_embd * 2 + style_dim, d_hid // 2, 1, batch_first=True, bidirectional=True)
         self.F0 = nn.ModuleList()
         self.F0.append(AdainResBlk1d(d_hid, d_hid, style_dim, dropout_p=dropout))
         self.F0.append(AdainResBlk1d(d_hid, d_hid // 2, style_dim, upsample=True, dropout_p=dropout))
@@ -523,43 +526,44 @@ class ProsodyPredictor(nn.Module):
         self.N.append(AdainResBlk1d(d_hid, d_hid, style_dim, dropout_p=dropout))
         self.N.append(AdainResBlk1d(d_hid, d_hid // 2, style_dim, upsample=True, dropout_p=dropout))
         self.N.append(AdainResBlk1d(d_hid // 2, d_hid // 2, style_dim, dropout_p=dropout))
-        
+
         self.F0_proj = nn.Conv1d(d_hid // 2, 1, 1, 1, 0)
         self.N_proj = nn.Conv1d(d_hid // 2, 1, 1, 1, 0)
 
-
-    def forward(self, texts, style, text_lengths, alignment, m):
+    def forward(self, texts, prosody, style, text_lengths, alignment, m):
+        prosody = self.embedding(prosody)
+        texts = torch.cat([texts, prosody], axis=1)
         d = self.text_encoder(texts, style, text_lengths, m)
-        
+
         batch_size = d.shape[0]
         text_size = d.shape[1]
-        
+
         # predict duration
         input_lengths = text_lengths.cpu().numpy()
         x = nn.utils.rnn.pack_padded_sequence(
             d, input_lengths, batch_first=True, enforce_sorted=False)
-        
+
         m = m.to(text_lengths.device).unsqueeze(1)
-        
+
         self.lstm.flatten_parameters()
         x, _ = self.lstm(x)
         x, _ = nn.utils.rnn.pad_packed_sequence(
             x, batch_first=True)
-        
+
         x_pad = torch.zeros([x.shape[0], m.shape[-1], x.shape[-1]])
 
         x_pad[:, :x.shape[1], :] = x
         x = x_pad.to(x.device)
-                
+
         duration = self.duration_proj(nn.functional.dropout(x, 0.5, training=self.training))
-        
+
         en = (d.transpose(-1, -2) @ alignment)
 
         return duration.squeeze(-1), en
-    
+
     def F0Ntrain(self, x, s):
         x, _ = self.shared(x.transpose(-1, -2))
-        
+
         F0 = x.transpose(-1, -2)
         for block in self.F0:
             F0 = block(F0, s)
@@ -569,12 +573,12 @@ class ProsodyPredictor(nn.Module):
         for block in self.N:
             N = block(N, s)
         N = self.N_proj(N)
-        
+
         return F0.squeeze(1), N.squeeze(1)
-    
+
     def length_to_mask(self, lengths):
         mask = torch.arange(lengths.max()).unsqueeze(0).expand(lengths.shape[0], -1).type_as(lengths)
-        mask = torch.gt(mask+1, lengths.unsqueeze(1))
+        mask = torch.gt(mask + 1, lengths.unsqueeze(1))
         return mask
     
 class DurationEncoder(nn.Module):
